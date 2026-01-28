4736080c04460f5ad0b139cb3f28ef99460d13d7
Aleksi <aletoivonen@gmail.com>
2026-01-28 16:39:15 +0200

    stuff

--- fish/.config/fish/functions/addkeys.fish
+++ fish/.config/fish/functions/addkeys.fish
@@ -1,7 +1,12 @@
 function addkeys --description 'add ssh keys'
-    fish_ssh_agent
-    ssh-add /home/shared/.ssh/omagithub.rsa
-    ssh-add /home/shared/.ssh/zgithub.rsa
-    ssh-add /home/shared/.ssh/worksourcetree.rsa
-    ssh-add /home/shared/.ssh/zgithub_2026
+  #!/bin/bash  
+
+  fish_ssh_agent
+
+    echo $HOME
+
+    for key in $HOME/.ssh/*.rsa
+      ssh-add $key
+      echo added key $key
+    end
   end
